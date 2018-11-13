4967c73280ca7674aaa777add66de19f064b294c
Daniel Gomes Mateus <daniel.analista@outlook.com>
2018-11-13 21:15:59 -0200

    Adicionando regras de negócio

--- project/src/app/attempts/attempts.component.ts
+++ project/src/app/attempts/attempts.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttemptsComponent implements OnInit {
 
+  public heartEmpty:string = '/assets/heart_empty.png';
+  public heartFull:string  = '/assets/heart_full.png';
+
   constructor() { }
 
   ngOnInit() {
--- project/src/app/top/top.component.ts
+++ project/src/app/top/top.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class TopComponent {
 
+    public title:string = 'Desenvolvimento WEB com Angular';
 }
